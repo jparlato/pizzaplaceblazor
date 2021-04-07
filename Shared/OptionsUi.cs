eaf16844ca9dfc6e122399121ffff18d96cd50ae
jparlato@mindspring.com <jparlato@mindspring.com>
2021-04-07 04:38:28 -0400

    initial commit

---
+++ Shared/OptionsUi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Shared
+{
+	public class OptionsUi
+	{
+		public bool ShowBasket { get; set; }
+	}
+}
---
+++ Shared/State.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Shared
+{
+	public class State
+	{
+		public Menu Menu { get; set; }
+		public Basket Basket { get; set; } = new Basket();
+		public OptionsUi UI { get; set; } = new OptionsUi();
+	}
+}
